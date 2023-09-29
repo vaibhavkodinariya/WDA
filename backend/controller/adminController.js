f9667f7876da5c4cc715bbbecd39f4dd66b18e29
vaibhavpatel312 <vaibhavkodinariya312@gmail.com>
2023-09-29 15:48:47 +0530

    Admin Status Feature Done

--- backend/controller/adminController.js
+++ backend/controller/adminController.js
@@ -1,4 +1,7 @@
 const asyncHandler = require("express-async-handler");
+const Status = require("../models/statusModel");
+const User = require("../models/userModel");
+const Website = require("../models/websiteModel");
 
 //@desc Get Website Status By Number
 //@Route /wda/admin/webSiteStatus/:contactNo
@@ -8,6 +11,53 @@ const getWebSiteStatusByNumber = asyncHandler(async (req, res) => {
   if (!contactNo) {
     return res.send({ success: false, message: "Invalid User" });
   } else {
+    const condition = {
+      ContactNo: contactNo,
+    };
+    const result = await User.aggregate([
+      {
+        $match: condition,
+      },
+      {
+        $lookup: {
+          from: "websites",
+          localField: "_id", // Field in the "orders" collection
+          foreignField: "userId", // Field in the "products" collection
+          as: "websites",
+        },
+      },
+    ]);
+
+    const statusDetails = [];
+    for (data of result[0]["websites"]) {
+      var promise = await new Promise((resolve) => {
+        const status = Status.find({ webSiteId: data["_id"].toString() });
+        resolve(status);
+      });
+      statusDetails.push(promise[0]);
+    }
+
+    const mergedData = statusDetails.map((status) => {
+      const matchingWebsite = result[0]["websites"].find((website) =>
+        website._id.equals(status.webSiteId)
+      );
+      if (matchingWebsite) {
+        // console.log(status["_id"]);
+        return {
+          statusName: status["statusName"],
+          webSiteId: status["webSiteId"].toString(),
+          domainName: matchingWebsite.domainName,
+        };
+      } else {
+        return status;
+      }
+    });
+    res.send({
+      success: true,
+      Name: result[0]["Name"],
+      ContactNo: result[0]["ContactNo"],
+      statusData: mergedData,
+    });
   }
 });
 
