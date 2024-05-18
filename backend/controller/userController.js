cb5fc6a2c13b523207ac8cb9ccedcaa1ac3a48b7
vaibhavpatel312 <vaibhavkodinariya312@gmail.com>
2024-05-18 21:56:12 +0530

    User Module Optimized

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -6,13 +6,11 @@ const path = require("path");
 const storage = require("node-persist");
 const Website = require("../models/websiteModel");
 const Query = require("../models/queryModel");
-
 const delay = 2 * 60 * 1000;
 const client = require("twilio")(
   process.env.TWILIO_ACCOUNT_SID,
   process.env.TWILIO_AUTH_TOKEN
 );
-
 const speakeasy = require("speakeasy");
 
 //@desc Login user
@@ -20,29 +18,27 @@ const speakeasy = require("speakeasy");
 //access Private
 const userLogin = asyncHandler(async (req, res) => {
   const { contactNumber, password, type } = req.body;
-
-  if (!contactNumber || !password || !type) {
-    res.send(
-      res.json({ success: false, message: "Please fill your Credentails" })
-    );
-  }
-  const userdata = await User.findOne({ ContactNo: contactNumber });
-  if (userdata && (await bcry.compare(password, userdata.Password))) {
-    if (type == "Admin") {
-      res.send({
-        success: true,
-        userId: userdata._id,
-      });
-    } else {
-      res.send({
-        success: true,
-        userID: userdata._id,
-        Name: userdata.Name,
-        ContactNo: userdata.ContactNo,
+  try {
+    if (!contactNumber || !password || !type) {
+      return res.status(400).send({
+        message: "Please fill your Credentails",
       });
     }
-  } else {
-    res.send({ success: false, message: "Incorrect Username or Password" });
+    const userdata = await User.findOne({ ContactNo: contactNumber });
+    if (userdata && (await bcry.compare(password, userdata.Password))) {
+      return type == "admin"
+        ? res.status(200).send({
+            userId: userdata._id,
+          })
+        : res.status(200).send({
+            userID: userdata._id,
+            Name: userdata.Name,
+            ContactNo: userdata.ContactNo,
+          });
+    }
+    return res.status(400).send({ message: "Incorrect Username or Password" });
+  } catch (error) {
+    return res.status(400).send({ message: error.message });
   }
 });
 
@@ -51,21 +47,17 @@ const userLogin = asyncHandler(async (req, res) => {
 //access Private
 const validedDomain = asyncHandler(async (req, res) => {
   const { domain } = req.params;
-
-  if (!domain) {
-    return res.send(
-      res.json({ success: false, message: "Please fill your Credentails" })
-    );
-  } else {
+  try {
+    if (!domain)
+      return res.status(400).send({ message: "Please fill your Credentails" });
     const validedDomain = domain.replace(/\s+/g, "");
     const isValid = await Website.findOne({
       domainName: `${validedDomain}.html`,
     });
-    if (isValid) {
-      return res.send({ success: true, message: "WebSite Exists" });
-    } else {
-      return res.send({ success: false, message: "WebSite Not Exists" });
-    }
+    if (isValid) return res.status(200).send({ message: "WebSite Exists" });
+    return res.status(400).send({ message: "WebSite Not Exists" });
+  } catch (error) {
+    return res.status(400).send({ message: error.message });
   }
 });
 
@@ -74,18 +66,16 @@ const validedDomain = asyncHandler(async (req, res) => {
 //access Private
 const sendOtp = asyncHandler(async (req, res) => {
   const { name, type, contactNo } = req.body;
-  if (!name || !type || !contactNo) {
-    return res.send({
-      success: false,
-      message: "Please Enter your Details Properly",
-    });
-  }
+  try {
+    if (!name || !type || !contactNo) {
+      return res.status(400).send({
+        message: "Please Enter your Details Properly",
+      });
+    }
+    const data = await User.findOne({ ContactNo: contactNo });
+    if (data)
+      return res.status(400).send({ message: "Already Registered Number" });
 
-  const data = await User.findOne({ ContactNo: contactNo });
-
-  if (data) {
-    res.send({ success: false, message: "Already Registered Number" });
-  } else {
     const secret = speakeasy.generateSecret({ length: 20 }); // Generate a secret key
     const otp = speakeasy.totp({
       secret: secret.base32,
@@ -108,7 +98,6 @@ const sendOtp = asyncHandler(async (req, res) => {
     setTimeout(() => {
       storage.removeItem(name);
     }, delay);
-
     await client.messages
       .create({
         body:
@@ -118,14 +107,15 @@ const sendOtp = asyncHandler(async (req, res) => {
         from: "+16672819468",
       })
       .then(() =>
-        res.send({
-          success: true,
+        res.status(200).send({
           message: "OTP has been Send To Your Entered Number",
         })
       )
       .catch((error) => {
-        res.send({ success: false, message: "Something Went Wrong" });
+        res.status(400).send({ message: error.message });
       });
+  } catch (error) {
+    return res.status(400).send({ message: error.message });
   }
 });
 
@@ -134,19 +124,14 @@ const sendOtp = asyncHandler(async (req, res) => {
 //access Private
 const verifyOtp = asyncHandler(async (req, res) => {
   const { otpCode, name } = req.body;
-  if (!otpCode) {
-    res.send({ success: false, message: "Send OTP Properly" });
-  } else {
-    try {
-      const data = await storage.getItem(name);
-      if (data["otp"] == otpCode) {
-        res.send({ success: true, message: "Otp Verified" });
-      } else {
-        res.send({ success: false, message: "Wrong OTP Please Try Again" });
-      }
-    } catch (e) {
-      res.send({ success: false, message: "Otp Expired Resend Again" });
-    }
+  if (!otpCode) return res.status(400).send({ message: "Send OTP Properly" });
+  try {
+    const data = await storage.getItem(name);
+    if (data["otp"] == otpCode)
+      return res.status(200).send({ success: true, message: "Otp Verified" });
+    return res.status(400).send({ message: "Wrong OTP Please Try Again" });
+  } catch (error) {
+    return res.status(400).send({ message: error.message });
   }
 });
 
@@ -155,37 +140,30 @@ const verifyOtp = asyncHandler(async (req, res) => {
 //access Private
 const registerUser = asyncHandler(async (req, res) => {
   const { name, password, confirmpassword } = req.body;
-  if (!password || !confirmpassword) {
-    res.send({
-      success: false,
-      message: "Please Enter Password or ConfirmPassword Properly",
-    });
-  } else {
+  try {
+    if (!password || !confirmpassword) {
+      return res.status(400).send({
+        message: "Please Enter Password or ConfirmPassword Properly",
+      });
+    }
     if (password != confirmpassword) {
-      res.send({
-        success: false,
+      return res.status(400).send({
         message: "Password and ConfirmPassword Doesn't Match",
       });
-    } else {
-      const salt = await bcry.genSalt(10);
-      const hashpassword = await bcry.hash(password, salt);
-      const data = await storage.getItem(name);
-      const userdata = await User.create({
-        Name: data["data"]["name"],
-        Type: data["data"]["type"],
-        ContactNo: data["data"]["contactNo"],
-        Password: hashpassword,
-      });
-      if (userdata) {
-        res.send(
-          res.json({
-            success: true,
-          })
-        );
-      } else {
-        res.send({ success: false, message: "Invalid User Data" });
-      }
     }
+    const salt = await bcry.genSalt(10);
+    const hashpassword = await bcry.hash(password, salt);
+    // const data = await storage.getItem(name);
+    const userdata = await User.create({
+      Name: name,
+      Type: "user",
+      ContactNo: 8155801818,
+      Password: hashpassword,
+    });
+    if (userdata) return res.sendStatus(201);
+    return res.status(400).send({ message: "Invalid User Data" });
+  } catch (error) {
+    return res.status(400).send({ message: error.message });
   }
 });
 
@@ -204,61 +182,58 @@ const updateUserProfile = asyncHandler(async (req, res) => {
     pincode,
     profileImagePath,
   } = req.body;
-
-  const dataProfile = await User.findOne({ ContactNo: contactNumber });
-  const imageBuffer = Buffer.from(profileImagePath, "base64");
-  const newPath = path.dirname(__dirname);
-
-  const folderpath = path.join(newPath, "userProfile");
-  const ImagePath = folderpath.replace(/\\/g, "/");
-  const newName = name.replace(/\s+/g, "");
-  if (fs.existsSync(`${ImagePath}/${dataProfile.Image}`)) {
-    if (`${newName}.jpg` != dataProfile.Image) {
-      fs.renameSync(
-        `${ImagePath}/${dataProfile.Image}`,
-        `${ImagePath}/${newName}.jpg`
-      );
+  try {
+    const dataProfile = await User.findOne({ ContactNo: contactNumber });
+    const imageBuffer = Buffer.from(profileImagePath, "base64");
+    const newPath = path.dirname(__dirname);
+    const folderpath = path.join(newPath, "userProfile");
+    const ImagePath = folderpath.replace(/\\/g, "/");
+    const newName = name.replace(/\s+/g, "");
+    if (fs.existsSync(`${ImagePath}/${dataProfile.Image}`)) {
+      if (`${newName}.jpg` != dataProfile.Image) {
+        fs.renameSync(
+          `${ImagePath}/${dataProfile.Image}`,
+          `${ImagePath}/${newName}.jpg`
+        );
+      } else {
+        if (imageBuffer != "") {
+          fs.unlinkSync(`${ImagePath}/${dataProfile.Image}`);
+          fs.writeFile(`${ImagePath}/${newName}.jpg`, imageBuffer, (err) => {
+            if (err) {
+              return res.status(400).send({
+                message: err.message,
+              });
+            }
+          });
+        }
+      }
     } else {
-      if (imageBuffer != "") {
-        fs.unlinkSync(`${ImagePath}/${dataProfile.Image}`);
-        fs.writeFile(`${ImagePath}/${newName}.jpg`, imageBuffer, (err) => {
-          if (err) {
-            return res.send({
-              success: false,
-              message: "SomeThing Went Wrong",
-            });
-          }
-        });
-      }
+      fs.writeFile(`${ImagePath}/${newName}.jpg`, imageBuffer, (err) => {
+        if (err) {
+          return res.status(400).send({
+            message: err.message,
+          });
+        }
+      });
     }
-  } else {
-    fs.writeFile(`${ImagePath}/${newName}.jpg`, imageBuffer, (err) => {
-      if (err) {
-        return res.send({
-          success: false,
-          message: "SomeThing Went Wrong",
-        });
-      }
-    });
-  }
-  const updateByNumber = { ContactNo: contactNumber };
-  const update = {
-    $set: {
-      Name: name,
-      Gender: gender,
-      DOB: dob,
-      Address: address,
-      City: city,
-      State: state,
-      Pincode: pincode,
-      Image: `${newName}.jpg`,
-    },
-  };
-  const result = await User.updateOne(updateByNumber, update);
-  if (result) {
-    return res.send({ success: true, message: "Profile Updated" });
-  } else {
-    return res.send({ success: false, message: "SomeThing Went Wrong" });
+    const updateByNumber = { ContactNo: contactNumber };
+    const update = {
+      $set: {
+        Name: name,
+        Gender: gender,
+        DOB: dob,
+        Address: address,
+        City: city,
+        State: state,
+        Pincode: pincode,
+        Image: `${newName}.jpg`,
+      },
+    };
+    const result = await User.updateOne(updateByNumber, update);
+    if (result) return res.status(200).send({ message: "Profile Updated" });
+    else return res.status(400).send({ message: "SomeThing Went Wrong" });
+  } catch (error) {
+    return res.status(400).send({ message: error.message });
   }
 });
 
@@ -267,11 +242,12 @@ const updateUserProfile = asyncHandler(async (req, res) => {
 //access Private
 const getUserProfile = asyncHandler(async (req, res) => {
   const { contactNo } = req.params;
-  if (!contactNo) {
-    return res.send({ success: false, message: "Invalid Profile" });
-  } else {
+  try {
+    if (!contactNo) return res.status(400).send({ message: "Invalid Profile" });
     const profile = await User.findOne({ ContactNo: contactNo });
-    res.send({ success: true, userProfile: profile });
+    res.status(200).send({ userProfile: profile });
+  } catch (error) {
+    return res.status(400).send({ message: error.message });
   }
 });
 
@@ -280,11 +256,12 @@ const getUserProfile = asyncHandler(async (req, res) => {
 //access Private
 const getUserWebsites = asyncHandler(async (req, res) => {
   const { userId } = req.params;
-  if (!userId) {
-    return res.send({ success: false, message: "Invalid User" });
-  } else {
+  try {
+    if (!userId) return res.status(400).send({ message: "Invalid User" });
     const fetchedData = await Website.find({ userId: userId });
-    res.send({ success: true, websiteDetails: fetchedData });
+    res.status(200).send({ websiteDetails: fetchedData });
+  } catch (error) {
+    res.status(400).send({ message: error.message });
   }
 });
 
@@ -293,23 +270,22 @@ const getUserWebsites = asyncHandler(async (req, res) => {
 //access Private
 const raiseQuery = asyncHandler(async (req, res) => {
   const { description, webSiteId, userId } = req.body;
-  if (!description || !webSiteId || !userId) {
-    return res.send({
-      success: false,
-      message: "Please Enter All Details",
-    });
-  } else {
+  try {
+    if (!description || !webSiteId || !userId) {
+      return res.status(400).send({
+        message: "Please Enter All Details",
+      });
+    }
     const isRaised = await Query.create({
       description: description,
       webSiteId: webSiteId,
       userId: userId,
       date: new Date(),
     });
-    if (isRaised) {
-      return res.send({ success: true, message: "Query Raised" });
-    } else {
-      return res.send({ success: false, message: "SomeThing Went Wrong" });
-    }
+    if (isRaised)
+      return res.status(200).send({ success: true, message: "Query Raised" });
+  } catch (error) {
+    return res.status(400).send({ message: error.message });
   }
 });
 
--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,6 @@ connectDB();
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use(express.json());
 app.use(cors());
 
 app.use("/wda", express.static("businessWebsite"));
