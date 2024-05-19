61a2bc912e06fbf250bbe80e752426657917ba68
vaibhavpatel312 <vaibhavkodinariya312@gmail.com>
2024-05-19 15:20:20 +0530

    WebSite Module Optimized

--- backend/controller/websiteController.js
+++ backend/controller/websiteController.js
@@ -11,38 +11,34 @@ const Status = require("../models/statusModel");
 //access Private
 const addImages = asyncHandler(async (req, res) => {
   const { businessName, imageID, imagePath } = req.body;
-
   if (!businessName || !imageID || !imagePath) {
-    return res.send({ success: false, message: "Something Went Wrong" });
-  } else {
+    return res
+      .status(400)
+      .send({ success: false, message: "Something Went Wrong" });
+  }
+  try {
     const imageBuffer = Buffer.from(imagePath, "base64");
-
     const newPath = path.dirname(__dirname);
-
     const businessNameToStore = businessName.replace(/\s+/g, "");
-
     const folderpath = path.join(
       newPath,
       "businessImages",
       businessNameToStore
     );
-
     const businessImagePath = folderpath.replace(/\\/g, "/");
     fs.mkdir(folderpath, { recursive: true }, (err) => {
-      if (err) {
-        return res.send({ success: false, message: "Something Went Wrong" });
-      } else {
+      if (err) return res.status(400).send({ message: err.message });
+      else {
         fs.writeFile(
           `${businessImagePath}/${imageID}.jpg`,
           imageBuffer,
           (err) => {
             if (err) {
-              return res.send({
-                success: false,
-                message: "Error saving image to Server",
+              return res.status(400).send({
+                message: err.message,
               });
             } else {
-              res.send({
+              res.status(200).send({
                 id: imageID,
                 url: `wda/Images/${businessNameToStore}/${imageID}.jpg`,
               });
@@ -51,6 +47,8 @@ const addImages = asyncHandler(async (req, res) => {
         );
       }
     });
+  } catch (error) {
+    res.status(400).send({ message: error.message });
   }
 });
 
@@ -69,75 +67,63 @@ const websiteRegister = asyncHandler(async (req, res) => {
     userId,
   } = req.body;
   if (!htmlFile || !webSiteName || !websiteType || !userId) {
-    return res.send({
-      success: false,
+    return res.status(400).send({
       message: "Please Fill All The Data",
     });
+  }
+  let isActive = await Website.findOne({ websiteName: webSiteName });
+  if (isActive) {
+    return res.send({
+      success: false,
+      message: "Website Already Registered",
+    });
   } else {
-    let isActive = await Website.findOne({ websiteName: webSiteName });
-    if (isActive) {
-      return res.send({
-        success: false,
-        message: "Website Already Registered",
-      });
-    } else {
+    try {
       const newPath = path.dirname(__dirname);
-
       const webSiteNameToStore = webSiteName.replace(/\s+/g, "");
-
       const folderpath = path.join(newPath, "businessWebsite");
-
       const htmlBuffer = Buffer.from(htmlFile, "base64");
-
       zlib.gunzip(htmlBuffer, (err, decompressedHtml) => {
-        if (err) {
-          console.error("Error decompressing HTML:", err);
-          return;
-        }
-
+        if (err) return res.status(400).send({ message: err.message });
         fs.writeFile(
           folderpath + `/` + `${webSiteNameToStore}.html`,
           decompressedHtml,
           "utf8",
           async (writeErr) => {
             if (writeErr) {
-              return res.send({
-                success: false,
-                message: "Error Registering Website",
+              return res.status(400).send({
+                message: writeErr.message,
+              });
+            }
+            const websiteRegisteration = await Website.create({
+              websiteName: webSiteName,
+              websiteType: websiteType,
+              dateOfIncorporation: dateOfIncorporation,
+              corporateIdentificationNo: corporateIdentificationNo,
+              taxDeductionAccNo: taxDeductionAccNo,
+              goodsAndServicesTax: goodsServiceTax,
+              domainName: `${webSiteNameToStore}.html`,
+              userId: userId,
+            });
+            if (websiteRegisteration) {
+              const insertedId = websiteRegisteration._id;
+              await Status.create({
+                statusName: "hosted",
+                webSiteId: insertedId,
+              });
+              return res.status(200).send({
+                message: "Website Registered",
+                domainName: webSiteName,
               });
             } else {
-              const websiteRegisteration = await Website.create({
-                websiteName: webSiteName,
-                websiteType: websiteType,
-                dateOfIncorporation: dateOfIncorporation,
-                corporateIdentificationNo: corporateIdentificationNo,
-                taxDeductionAccNo: taxDeductionAccNo,
-                goodsAndServicesTax: goodsServiceTax,
-                domainName: `${webSiteNameToStore}.html`,
-                userId: userId,
+              return res.status(400).send({
+                message: "Website Not Registered",
               });
-              if (websiteRegisteration) {
-                const insertedId = websiteRegisteration._id;
-                await Status.create({
-                  statusName: "hosted",
-                  webSiteId: insertedId,
-                });
-                return res.send({
-                  success: true,
-                  message: "Website Registered",
-                  domainName: webSiteName,
-                });
-              } else {
-                return res.send({
-                  success: false,
-                  message: "Something Went Wrong",
-                });
-              }
             }
           }
         );
       });
-    }
+    } catch (error) {}
   }
 });
 
@@ -146,47 +132,43 @@ const websiteRegister = asyncHandler(async (req, res) => {
 //access Private
 const updateRegisteredWebsite = asyncHandler(async (req, res) => {
   const { updatedWebsiteCode, webSiteName } = req.body;
-  if (!updatedWebsiteCode || !webSiteName) {
-    return res.send({ success: false, message: "Please Select Your WebSite" });
-  } else {
-    const newPath = path.dirname(__dirname);
+  if (!updatedWebsiteCode || !webSiteName)
+    return res.status(400).send({ message: "Please Select Your WebSite" });
 
-    const webSiteNameToStore = webSiteName.replace(/\s+/g, "");
+  const newPath = path.dirname(__dirname);
 
-    const folderpath = path.join(newPath, "businessWebsite");
+  const webSiteNameToStore = webSiteName.replace(/\s+/g, "");
 
-    const htmlBuffer = Buffer.from(updatedWebsiteCode, "base64");
+  const folderpath = path.join(newPath, "businessWebsite");
 
-    zlib.gunzip(htmlBuffer, (err, decompressedHtml) => {
-      if (err) {
-        console.error("Error decompressing HTML:", err);
-        return;
-      }
-      fs.unlinkSync(
-        folderpath + `\\` + `${webSiteNameToStore}.html`,
-        decompressedHtml,
-        "utf8"
-      );
-      fs.writeFile(
-        folderpath + `\\` + `${webSiteNameToStore}.html`,
-        decompressedHtml,
-        "utf8",
-        (writeErr) => {
-          if (writeErr) {
-            return res.send({
-              success: false,
-              message: "Error Updating Website",
-            });
-          } else {
-            return res.send({
-              success: true,
-              message: "Your Website Updated..",
-            });
-          }
+  const htmlBuffer = Buffer.from(updatedWebsiteCode, "base64");
+
+  zlib.gunzip(htmlBuffer, (err, decompressedHtml) => {
+    if (err) {
+      console.error("Error decompressing HTML:", err);
+      return;
+    }
+    fs.unlinkSync(
+      folderpath + `\\` + `${webSiteNameToStore}.html`,
+      decompressedHtml,
+      "utf8"
+    );
+    fs.writeFile(
+      folderpath + `\\` + `${webSiteNameToStore}.html`,
+      decompressedHtml,
+      "utf8",
+      (writeErr) => {
+        if (writeErr) {
+          return res.status(400).send({
+            message: writeErr.message,
+          });
         }
-      );
-    });
-  }
+        return res.status(200).send({
+          message: "Your Website Updated.",
+        });
+      }
+    );
+  });
 });
 
 //@desc Update Website
@@ -194,20 +176,21 @@ const updateRegisteredWebsite = asyncHandler(async (req, res) => {
 //access Private
 const uploadTemplateDetails = asyncHandler(async (req, res) => {
   const uploadedFile1 = req.files["template"][0];
-  const htmlName = uploadedFile1.originalname;
+  if (!uploadedFile1)
+    return res.status(400).send({ message: "Please Send Website" });
 
+  const htmlName = uploadedFile1.originalname;
   const uploadedFile2 = req.files["templateImage"][0];
   const gifName = uploadedFile2.originalname;
-  const { name, extension } = gifName.split(".");
+  const { name } = gifName.split(".");
   const templateInsert = await Template.create({
     templatePath: `/wda/templates/${htmlName}`,
     imageName: name,
   });
-  if (templateInsert) {
-    res.send({ succes: true, message: "Files uploaded successfully" });
-  } else {
-    res.send({ succes: false, message: "Something Went Wrong" });
-  }
+  if (templateInsert)
+    return res.status(200).send({ message: "Files uploaded successfully" });
+
+  return res.status(400).send({ message: "Website Not Updated" });
 });
 module.exports = {
   addImages,
--- backend/routes/websiteRoutes.js
+++ backend/routes/websiteRoutes.js
@@ -11,11 +11,9 @@ const router = express.Router();
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     const extension = file.originalname.split(".");
-    if (extension[1] === "html") {
+    if (extension[1] === "html")
       cb(null, "E:/Projects/WDA/backend/businessTemplates/");
-    } else {
-      cb(null, "E:/Projects/WDA/backend/templateImages/");
-    }
+    else cb(null, "E:/Projects/WDA/backend/templateImages/");
   },
   filename: (req, file, cb) => {
     cb(null, file.originalname); // Use the original file name
@@ -23,7 +21,6 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
-
 router.post("/addImages", addImages);
 router.post("/websiteRegister", websiteRegister);
 router.put("/updateWebsite", updateRegisteredWebsite);
